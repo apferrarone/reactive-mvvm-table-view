276e72b18c00217cd287d7e66cc452a0670e6c12
Andrew Ferrarone <apferrarone@gmail.com>
2026-02-09 22:02:30 -0800

    Enforces .lightContent status bars at all times

--- ProfileTableViewArchitecture/Supporting Files/SceneDelegate.swift
+++ ProfileTableViewArchitecture/Supporting Files/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         // setup root navigation controller
-        self.topNavigationController = UINavigationController()
+        self.topNavigationController = StatusBarNavigationController()
         self.topNavigationController.navigationBar.barStyle = .black
         self.topNavigationController.navigationBar.prefersLargeTitles = false
         
@@ -32,5 +32,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
         self.coordinator = Coordinator(rootViewController: self.topNavigationController)
         self.coordinator.start()
     }
+}
+
+// Necessary to honor .lightContent status bar at all times
+final class StatusBarNavigationController: UINavigationController {
+    override var childForStatusBarStyle: UIViewController? { topViewController }
+    override var childForStatusBarHidden: UIViewController? { topViewController }
 }
 
